ccb02b548a4bffc98edc1311dbbff80edc959c3b
seanmajorpayne <seanmajorpayne@gmail.com>
2021-02-25 20:53:52 +0900

    Adding requirements

--- app/index/routes.py
+++ app/index/routes.py
@@ -6,6 +6,6 @@ from app.index import bp
 @bp.route("/")
 @bp.route("/index")
 def index():
-    variable = 1
-    variable = 2
+    views = 1
+    subscribers = 2
     return render_template("/index.html", variable=views, variable2=subscribers, year='2020')
