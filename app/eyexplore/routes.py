eaacc62306c2359909b528e566d00a7b13649743
seanmajorpayne <seanmajorpayne@gmail.com>
2021-02-25 20:08:34 +0900

    Creating index page

--- app/eyexplore/routes.py
+++ app/eyexplore/routes.py
@@ -2,16 +2,18 @@ from flask import render_template
 from app.google import google_api
 from app.eyexplore.eyexplore import Eyexplore
 
+from app.eyexplore import bp
+
 EYEXPLORE = Eyexplore()
 
-@app.route('/eyexploreCaseStudy')
+@bp.route('/eyexploreCaseStudy')
 def eyexplore_case():
     video_count = EYEXPLORE.get_video_count()
     views = EYEXPLORE.get_views()
     subscribers = EYEXPLORE.get_subscribers()
     video_count, views, subscribers
     return render_template(
-        'eyexploreCaseStudy.html',
+        'eyexplore/eyexploreCaseStudy.html',
         video_count=video_count,
         views=views,
         subscribers=subscribers,
